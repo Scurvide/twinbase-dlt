9c96008a61e0167f7043fb249bfc072daf35ccbc
Jani Hietala <janimhietala@gmail.com>
2023-03-14 13:02:44 +0200

    Compare twin hash to one found in dlt before creating transaction

--- .github/store-hash-to-dlt.py
+++ .github/store-hash-to-dlt.py
@@ -5,7 +5,7 @@ import hashlib
 from web3 import Web3
 from eth_account.signers.local import LocalAccount
 
-DLT_TYPE = "ethereum"
+DLT_TYPE = os.environ["DLT_TYPE"]
 DLT_PROVIDER = Web3.HTTPProvider(os.environ["DLT_HTTP_PROVIDER"])
 DLT_PRIVATE_KEY = os.environ["DLT_PRIVATE_KEY"]
 DLT_GAS_PROVIDED = int(os.environ["DLT_GAS_PROVIDED"])
@@ -27,18 +27,23 @@ def hash_json_file(document_path: str) -> str:
     return "0x" + hashlib.md5(json_bytes).hexdigest()
 
 
-def hash_changed(twin_hash: str, twin_hash_info_file: str) -> bool:
+def hash_requires_update(dlt: Web3, twin_hash: str, twin_hash_info_file: str) -> bool:
     """Check if twin document hash has changed from the one stored in DLT."""
 
-    # Attempt to find previous hash from transaction file
     try:
+        # Attempt to find transaction hash from transaction file
         with open(twin_hash_info_file) as hash_info:
-            dlt_stored_hash = json.load(hash_info)["hash"]
-    except (FileNotFoundError, KeyError):
-        return True
-    # Compare hashes
-    if int(twin_hash, 16) == int(dlt_stored_hash, 16):
-        return False
+            transaction_hash = json.load(hash_info)["transaction_hash"]
+        # Get transaction from DLT
+        transaction = dlt.eth.get_transaction(transaction_hash)
+        # Compare hashes
+        if int(twin_hash, 16) == int(transaction["input"], 16):
+            # Hash has not changed from DLT, no update needed
+            return False
+    except (FileNotFoundError, KeyError, ValueError):
+        # File or hash not found, or hash is invalid
+        pass
+    # Hash requires an update
     return True
 
 
@@ -50,8 +55,7 @@ def save_transaction_info(
     # Collect transaction info
     transaction_info = {
         "dlt": DLT_TYPE,
-        "provider": str(DLT_PROVIDER),
-        "json_hash": twin_hash,
+        "twin_json_hash": twin_hash,
         "transaction_hash": transaction_hash,
     }
     with open(twin_hash_info_file, "w+") as file:
@@ -59,38 +63,38 @@ def save_transaction_info(
         json.dump(transaction_info, file, indent=4)
 
 
-def submit_twin_hash_to_dlt(twin_hash: str) -> str:
+def submit_twin_hash_to_dlt(dlt: Web3, twin_hash: str) -> str:
     """Submit hash to DLT, returns transaction hash."""
 
-    # Connect to the DLT network
-    web3 = Web3(DLT_PROVIDER)
-    if not web3.is_connected():
-        raise ConnectionError("Could not connect to DLT Provider:", DLT_PROVIDER)
-
     # Create an account helper with private key
-    account: LocalAccount = web3.eth.account.from_key(DLT_PRIVATE_KEY)
+    account: LocalAccount = dlt.eth.account.from_key(DLT_PRIVATE_KEY)
 
     # Create the transaction object
     transaction = {
         "to": None,  # This is a contract creation transaction, so there is no recipient
         "from": account.address,
         "gas": DLT_GAS_PROVIDED,
-        "gasPrice": web3.eth.gas_price,
-        "nonce": web3.eth.get_transaction_count(account.address),
+        "gasPrice": dlt.eth.gas_price,
+        "nonce": dlt.eth.get_transaction_count(account.address),
         "data": twin_hash,
     }
 
     # Sign the transaction with the account's private key
-    signed_transaction = web3.eth.account.sign_transaction(
+    signed_transaction = dlt.eth.account.sign_transaction(
         transaction, private_key=account.key
     )
 
     # Broadcast the transaction to the network
-    transaction_hash = web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+    transaction_hash = dlt.eth.send_raw_transaction(signed_transaction.rawTransaction)
     return transaction_hash.hex()
 
 
 def main() -> None:
+    # Connect to the DLT network
+    dlt = Web3(DLT_PROVIDER)
+    if not dlt.is_connected():
+        raise ConnectionError("Could not connect to DLT Provider:", DLT_PROVIDER)
+
     # Loop through twin folders, collect hashes and write them to DLT
     for folder in os.listdir(TWIN_DOCUMENT_FOLDERS):
         twin_folder = f"{TWIN_DOCUMENT_FOLDERS}/{folder}"
@@ -111,12 +115,12 @@ def main() -> None:
         twin_hash_file = f"{twin_folder}/{HASH_INFO_FILE}"
 
         # Check if hash needs to be updated to the DLT
-        if not hash_changed(twin_hash, twin_hash_file):
-            print(f"Twin document hash is unchanged, skipping: {twin_document}")
+        if not hash_requires_update(dlt, twin_hash, twin_hash_file):
+            print(f"Twin hash is unchanged from DLT, skipping: {twin_document}")
             continue
 
         print(f"Submitting twin document hash to DLT: {twin_document}")
-        transaction_hash = submit_twin_hash_to_dlt(twin_hash)
+        transaction_hash = submit_twin_hash_to_dlt(dlt, twin_hash)
 
         # Save transaction info to hash file for reference
         save_transaction_info(twin_hash, transaction_hash, twin_hash_file)
